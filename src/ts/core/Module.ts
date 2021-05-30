e4738f3522cc7e9cf6ced439dcf13b7b8f801932
Boby <40067018+bobyclaws@users.noreply.github.com>
2021-05-29 20:46:51 -0700

    added type info for Module.js, plus removing some functions, until improved/replaced.

--- src/ts/core/Module.ts
+++ src/ts/core/Module.ts
@@ -1,4 +1,5 @@
 import HBot from "./HBot";
+import { Message } from "discord.js";
 
 
 export default class Module {
@@ -11,21 +12,22 @@ export default class Module {
         this.commandName = "moduleName [gets overriden]";
     }
 
-    processInteraction() {
-        // this.log("method processInteraction() not implemented, skipping");
-    }
+    // processInteraction() {
+    //     // this.log("method processInteraction() not implemented, skipping");
+    // }
 
-    processCommand(msg: any, args: string[]) {
+
+    processCommand(msg: Message, args: string[]) {
         // this.log("method processCommand() not implemented, skipping");
     }
 
-    processMessage() {
+    processMessage(msg: Message) {
         // this.log("method processMessage() not implemented, skipping");
     }
 
-    log(...msg: any[]) {
-        console.log(`[${this.constructor.name} module]`, ...msg);
-    }
+    // log(...msg: any[]) {
+    //     console.log(`[${this.constructor.name} module]`, ...msg);
+    // }
 
 }
 
