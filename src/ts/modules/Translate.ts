ee0b7392c0253af06efe027ba0dc027c8bf4fe0d
Boby <40067018+bobyclaws@users.noreply.github.com>
2021-09-11 15:58:10 +0530

    Update Translate.ts

--- src/ts/modules/Translate.ts
+++ src/ts/modules/Translate.ts
@@ -1,4 +1,4 @@
-import { Message, MessageEmbed } from "discord.js";
+import { Message, MessageEmbed, TextChannel, Webhook } from "discord.js";
 import SBot from "../core/SBot";
 import Module from "../core/Module";
 
@@ -26,26 +26,80 @@ export default class Say extends Module {
         }
 
         let message = args.join(" ");
-
         let result = await this.translateText(message, target as string);
+        this.log("tranlsated text:", result);
 
-        let embed = new MessageEmbed();
-        embed.setDescription(result);
-        embed.setAuthor(msg.author.username, msg.author.avatarURL() as string);
-        embed.setColor("#eca861");
-        this.log(result);
-        msg.channel.send({embeds: [embed]});
-     
+
+        // let embed = new MessageEmbed();
+        // embed.setDescription(result);
+        // embed.setAuthor(msg.author.username, msg.author.avatarURL() as string);
+        // embed.setColor("#eca861");
+        // msg.channel.send({embeds: [embed]});
+
+
+        msg.delete();
+
+        // fetch this channel's webhook. if not present create it.
+        let webhook = await this.fetchOrCreateWebhook(msg.channel as TextChannel);
+
+        await webhook.edit({
+            avatar: msg.author.avatarURL() as string,
+        });
+
+        
+        webhook.sendSlackMessage({
+            'username': (await msg.guild!.members.fetch(msg.author.id)).nickname ||  msg.author.username,
+            'text': result
+        }).catch(console.error);
         
       
     }
+
+
+
+
 
     async translateText(text: string, language: string) {
 
         let [translation]: any = await translate.translate(text, language);
         return translation
         
-      }
+    }
+
+    async createWebhook(channel: TextChannel): Promise<Webhook> {
+        let webhook = await channel.createWebhook("Translate - " + channel.name, {
+        })
+        this.log(`Created webhook:  ${webhook.name} [${webhook.id}]`);
+        return webhook;
+    }
+
+    async fetchOrCreateWebhook(channel: TextChannel): Promise<Webhook> {
+        this.log("fetching webhooks for channel: ", channel.name);
+        let webhooks = await channel.fetchWebhooks();
+
+        let webhook: Webhook | null = null;
+
+        webhooks.forEach(wh => {
+
+            if(wh.name.startsWith("Translate")) {
+                this.log("found a Translate webhook");
+                webhook = wh;
+                this.log(`fetched webhook: ${webhook.name} [${webhook.id}]`);
+            }
+        });
+
+
+        
+        if(webhook == null) {
+            this.log("no Translate webhooks found");
+            this.log("creating a Translate webhook");
+            webhook = await this.createWebhook(channel);
+        
+        }
+        
+      
+        return webhook;
+    }
       
 
 
